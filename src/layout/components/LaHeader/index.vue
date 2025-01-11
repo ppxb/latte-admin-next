96a161d60526dd56db81c16646e111b67c9f8b75
ppxb <317842449@qq.com>
2025-01-11 13:44:03 +0800

    chore: update header icon style

--- src/layout/components/LaHeader/index.vue
+++ src/layout/components/LaHeader/index.vue
@@ -9,23 +9,27 @@ defineOptions({
 <template>
   <div class="h-12 flex items-center justify-between px-4 border-b border-gray-200 dark:border-gray-800">
     <div>123</div>
-    <div class="flex items-center gap-2">
+    <div class="flex items-center">
       <el-button
         circle
         class="!flex items-center justify-center"
         @click="toggleFullscreen"
       >
-        <IconifyIconOnline
-          :icon="isFullscreen ? 'ri:fullscreen-exit-line' : 'ri:fullscreen-line'"
-        />
+        <IconifyIconOnline :icon="isFullscreen ? 'ri:fullscreen-exit-line' : 'ri:fullscreen-line'" width="18" />
       </el-button>
       <el-button
         class="!flex items-center justify-center"
         circle
         @click="toggleDark()"
       >
-        <IconifyIconOnline :icon="isDark ? 'ri:sun-line' : 'ri:moon-line'" />
+        <IconifyIconOnline :icon="isDark ? 'ri:sun-line' : 'ri:moon-line'" width="18" />
       </el-button>
     </div>
   </div>
 </template>
+
+<style scoped lang="scss">
+.el-button {
+ border: none;
+}
+</style>
